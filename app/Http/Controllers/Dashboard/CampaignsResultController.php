eea954e7d7ad01906a98a567606e02de42094db2
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-04-22 23:09:42 +0200

    firat commit

--- app/Http/Controllers/Dashboard/CampaignsResultController.php
+++ app/Http/Controllers/Dashboard/CampaignsResultController.php
@@ -10,37 +10,16 @@ class CampaignsResultController extends Controller
 {
     public function show(Request $request)
     {
-        // Get the count of all campaign visits
-        $count_campaign = CampaignVisit::count();
+         $count_campaign = CampaignVisit::count(); // Get the count of blogs
+         $this->authorize('view_campaign');
 
-        // Ensure the user is authorized to view campaigns
-        $this->authorize('view_campaign');
+        if ($request->ajax())
+        {
+            $data = getModelData( model: new CampaignVisit(), andsFilters: [ [ 'campaign_id' , '=' , $request['campaign_id'] ] ],relations:['campaign' => [ 'id' , 'campaign_name' ] ] );
 
-        // If it's an AJAX request, return JSON data
-        if ($request->ajax()) {
-            // Get campaign_id from the request if it's provided
-            $campaign_id = $request->get('campaign_id');
-
-            // Build the query for CampaignVisit, filtered by campaign_id if available
-            $query = CampaignVisit::query();
-
-            if ($campaign_id) {
-                // Filter by campaign_id if it's passed in the request
-                $query->where('campaign_id', $campaign_id);
-            }
-
-            // Fetch the data with the related 'campaign' data
-            $data = getModelData(
-                model: $query,
-                relations: ['campaign' => ['id', 'campaign_name']]
-            );
-
-            // Return the data in JSON format
             return response()->json($data);
         }
 
-        // Otherwise, load the view and pass necessary data
-        return view('dashboard.campaigns.show', compact('count_campaign'));
+        return view('dashboard.campaigns.show' ,compact('count_campaign'));
     }
-
 }
