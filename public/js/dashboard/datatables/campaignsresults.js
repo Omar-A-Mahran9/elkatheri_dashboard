06dcc71936b1740f6b9e153877a79f549dfbe37f
Omar-A-Mahran9 <omar.a.m.mahran@gmail.com>
2025-04-22 20:05:39 +0200

    firat commit

--- public/js/dashboard/datatables/campaignsresults.js
+++ public/js/dashboard/datatables/campaignsresults.js
@@ -42,84 +42,13 @@ let KTDatatable = (function () {
                 { data: "created_at" },
                 { data: null },
             ],
-            columnDefs: [
-
-            ],
+            columnDefs: [],
         });
 
         table = datatable.$;
 
         datatable.on("draw", function () {
-            handleDeleteRows();
             KTMenu.createInstances();
-        });
-    };
-
-    // general search in datatable
-    let handleSearchDatatable = () => {
-        $("#general-search-inp").keyup(function () {
-            datatable.search($(this).val()).draw();
-        });
-    };
-
-    // Filter Datatable
-    let handleFilterDatatable = () => {
-        $(".filter-datatable-inp").each((index, element) => {
-            $(element).change(function () {
-                let columnIndex = $(this).data("filter-index"); // index of the searching column
-
-                datatable.column(columnIndex).search($(this).val()).draw();
-            });
-        });
-    };
-
-    // Delete record
-    let handleDeleteRows = () => {
-        $(".delete-row").click(function () {
-            let rowId = $(this).data("row-id");
-            let type = $(this).data("type");
-
-            deleteAlert(type).then(function (result) {
-                if (result.value) {
-                    loadingAlert(translate("deleting now ..."));
-
-                    $.ajax({
-                        method: "delete",
-                        headers: {
-                            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr(
-                                "content"
-                            ),
-                        },
-                        url: "/dashboard/campaigns/" + rowId,
-                        success: () => {
-                            setTimeout(() => {
-                                successAlert(
-                                    `${
-                                        translate("You have deleted the") +
-                                        " " +
-                                        type +
-                                        " " +
-                                        translate("successfully !")
-                                    } `
-                                ).then(function () {
-                                    datatable.draw();
-                                });
-                            }, 1000);
-                        },
-                        error: (err) => {
-                            if (err.hasOwnProperty("responseJSON")) {
-                                if (
-                                    err.responseJSON.hasOwnProperty("message")
-                                ) {
-                                    errorAlert(err.responseJSON.message);
-                                }
-                            }
-                        },
-                    });
-                } else if (result.dismiss === "cancel") {
-                    errorAlert(translate("was not deleted !"));
-                }
-            });
         });
     };
 
